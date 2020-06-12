0bfa790207f52a2261016fe2c5c7e154a30a3db7
Drew Paul <arpman41@gmail.com>
2020-06-12 14:20:27 -0400

    Initialized React frontend

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ConsumerHome from './containers/Cardholder/Home';
+
+function App() {
+  return (
+    <ConsumerHome />
+  );
+}
+
+export default App;
---
+++ client/src/containers/Cardholder/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Cardholder.css';
+
+function Home(props) {
+    return (
+        <div className='home'>
+            Visa Cardholder Home
+        </div>
+    )
+}
+
+export default Home;
+
