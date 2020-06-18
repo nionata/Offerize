e5269ee65a367ffa6a90016ba26b24e80f826576
Drew Paul <arpman41@gmail.com>
2020-06-18 18:18:13 -0400

    Added main input box with input fields

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CardholderHome from './containers/Cardholder/CardholderHome';
 
+import 'antd/dist/antd.css';
+
 function App() {
   return (
     <CardholderHome />
--- client/src/containers/Cardholder/CardholderHome.js
+++ client/src/containers/Cardholder/CardholderHome.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { Input, Select, Button } from 'antd';
 
 import Header from '../../components/Header.js';
 import Footer from '../../components/Footer.js';
 
+const { Option } = Select;
+
 function CardholderHome(props) {
+
+    function handleChange() {
+
+    }
+
     return (
         <>
             <Header />
-            <div style={{ height: '1000px' }} />
+            <div className='inputBox'>
+                <div className='inputBoxHeader'>
+                    Find offers from your favorite local businesses.
+                </div>
+                <div className='inputBoxFields'>
+                    <div className='inputBoxFieldsRow'>
+                        Find
+                        <Select defaultValue="Restaurants" style={{ width: '200px' }} size='large' onChange={handleChange}>
+                            <Option value="Restaurants">Restaurants</Option>
+                            <Option value="Bars">Bars</Option>
+                            <Option value="Dentists">Dentists</Option>
+                        </Select>
+                    </div>
+                    <div className='inputBoxFieldsRow'>
+                        Near
+                        <Input placeholder='Zip code' style={{ width: '200px' }} size='large' />
+                    </div>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button className='inputSearchButton' type='primary'>
+                        Search
+                    </Button>
+                </div>
+            </div>
+            <div style={{ height: '200px' }} />
             <Footer />
         </>
     )
