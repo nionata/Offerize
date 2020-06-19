0b4da4b81c5f96c501ed93e5b6ddcededac4fcff
Drew Paul <arpman41@gmail.com>
2020-06-19 15:22:22 -0400

    Added Google Maps to Cardholder Page

--- client/src/containers/Cardholder/CardholderHome.js
+++ client/src/containers/Cardholder/CardholderHome.js
@@ -1,45 +1,22 @@
 import React from 'react';
-import { Input, Select, Button } from 'antd';
 
 import Header from '../../components/Header.js';
 import Footer from '../../components/Footer.js';
-
-const { Option } = Select;
+import Map from './Map.js';
+import InputBox from './InputBox.js';
 
 function CardholderHome(props) {
 
-    function handleChange() {
-
+    function selectChange() {
     }
 
     return (
         <>
             <Header />
-            <div className='inputBox'>
-                <div className='inputBoxHeader'>
-                    Find offers from your favorite local businesses.
-                </div>
-                <div className='inputBoxFields'>
-                    <div className='inputBoxFieldsRow'>
-                        Find
-                        <Select defaultValue="Restaurants" style={{ width: '200px' }} size='large' onChange={handleChange}>
-                            <Option value="Restaurants">Restaurants</Option>
-                            <Option value="Bars">Bars</Option>
-                            <Option value="Dentists">Dentists</Option>
-                        </Select>
-                    </div>
-                    <div className='inputBoxFieldsRow'>
-                        Near
-                        <Input placeholder='Zip code' style={{ width: '200px' }} size='large' />
-                    </div>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Button className='inputSearchButton' type='primary'>
-                        Search
-                    </Button>
-                </div>
+            <div style={{ position: 'relative' }}>
+                <Map />
+                <InputBox />
             </div>
-            <div style={{ height: '200px' }} />
             <Footer />
         </>
     )
